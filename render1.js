a93fb0c71f1f456b0c415ea0b51ddd6b104ec4b4
andalusm <andalus0220@gmail.com>
2023-12-04 13:44:24 +0200

    Now index does it normally while index-copy does everything with handlebars

--- render1.js
+++ render1.js
@@ -19,11 +19,16 @@ const Renderer = function () {
 
 
     const renderPosts = function (posts) {
-         $("#posts").empty()
-         for (const p of posts) {
-             _renderPost(p, $("#posts"))
-         }
-        
+        // $("#posts").empty()
+        // for (const p of posts) {
+        //     _renderPost(p, $("#posts"))
+        // }
+        $("#posts").empty()
+        const source = $('#posts-template').html();
+        const template = Handlebars.compile(source);
+        let newHTML = template({ posts });
+        $("#posts").append(newHTML)
+
 
     }
 
